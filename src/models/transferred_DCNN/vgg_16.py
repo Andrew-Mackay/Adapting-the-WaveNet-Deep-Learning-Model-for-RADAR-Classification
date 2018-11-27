f4a20304c66aad5473f51a871009d84f41811fc3
Andrew Mackay <2192793m@student.gla.ac.uk>
2018-11-27 20:09:07 +0000

    debugged leave_one_user_out_cnn_64_128

---
+++ src/models/transferred_DCNN/vgg_16.py
@@ -0,0 +1,20 @@
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+
+
+def make_model(img_rows, img_cols, nb_classes):
+    model = VGG16(weights = "imagenet", include_top=False, input_shape=(3, img_rows, img_cols))
+    x = model.output
+    x = Flatten()(x)
+    # x = Dense(512, activation="relu")(x)
+    x = Dense(1024, activation="relu")(x)
+    x = Dropout(0.5)(x)
+    # x = Dense(512, activation="relu")(x)
+    x = Dense(1024, activation="relu")(x)
+    predictions = Dense(nb_classes, activation="softmax")(x)
+    model = Model(input=model.input, output = predictions)
+
+    return model
